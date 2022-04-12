fc5515dcd50e3810af63fc5bee9d08275b4ac4fb
Rasmus Rossetti <73909140+RasmusRossetti@users.noreply.github.com>
2022-04-12 09:05:12 +0200

    Update script.js

--- PokemonBackend/script.js
+++ PokemonBackend/script.js
@@ -1,9 +1,8 @@
-//Funktion för att hämta data från Strapi CMS
+
 async function getDataFromStrapi() {
-  //Url till Strapi.js API för att hämta alla Pokemons
+  
   let url = "http://localhost:1337/api/pokemons"
 
-  //Hämtar JSON från API och konverterar det till JS objekt
   let stringResponse = await fetch(url)
   let myObjekt = await stringResponse.json()
 
@@ -11,81 +10,64 @@ async function getDataFromStrapi() {
 
   let output = "<table>"
 
-  //Checkar om det är ett eller flera objekt som hämtas
-  //Kan undvikas genom flera funktioner; en för alla och en för unik
+  
   if (Array.isArray(myObjekt.data)) {
-    //Anropa generateRow för att skapa en HEader-rad
+    
     output += generateRow(myObjekt.data[0].attributes, null, true)
 
-    //Skapar en ForEach loop för varje elemet i Data-arrayen
+    
     myObjekt.data.forEach((element) => {
-      //Gör en pekare till attribut objektet
+      
       let obj = element.attributes
 
-      /*for (x in obj) {
-                console.log( x + ": " + obj[x]);
-            }*/
-
-      //Skriver Output string
-      //document.write(`Namn: ${attr.name}`);
+    
       output += generateRow(obj, element.id, false)
     })
   } else {
-    //Gör en pekare till attribut objektet
+    
     let obj = myObjekt.data.attributes
-    /*for (x in obj) {
-            console.log( x + ": " + obj[x]);
-        }*/
+    
 
-    //Skapa en Header Rad
     output += generateRow(obj, null, true)
 
-    //Skriver Output string
     output += generateRow(obj, myObjekt.data.id, false)
   }
 
-  //Avsluta <table> tag
+ 
 
   output += "</table>"
 
-  //Skriver ut Output string till div-element
-  //document.write(output);
+
   document.getElementById("pokemonFetched").innerHTML = output
 }
 
-//Funktion för att hämta Token för användare
-//Om en Token hämtas så betyder det att user/password är korrekt skrivet
+
 async function getToken() {
-  /*
-    1. Göra ett inloggningsförsök för att få en Token returnerad
-    2. Sammla data och skapa ett objekt av dessa
-    3. Skicka iväg JSON till API
-    */
+
 
   let valid = true
 
-  //Validera användarnamn och lösenord!
+
   if (!validateLogin()) valid = false
 
-  //Validera PokemonData
+  
   if (!validatePokemon()) valid = false
 
   if (!valid) return null
 
-  //Url till Strapi.js UserList
+ 
   const urlUser = "http://localhost:1337/api/auth/local/"
 
   const user = document.getElementById("user").value
   const pass = document.getElementById("pass").value
 
-  //Skapar ett objekt av det användarnamn och lösenord som user har skrivit in i fält.
+ 
   let userObject = {
     identifier: user,
     password: pass
   }
 
-  //Anropar API med inloggningsdata.
-  //Inkluderar Method och Headers
+
   let userResponse = await fetch(urlUser, {
     method: "POST",
     headers: {
@@ -94,16 +76,14 @@ async function getToken() {
     body: JSON.stringify(userObject)
   })
 
-  //Konverterar API response JSON string till ett objekt
+
   let userJson = await userResponse.json()
   console.log(userJson)
 
-  //Kontrollerar om objektet har Token.
-  //Token ligger under attribut jwt
-  //Om så; inloggning är korrekt. Fortsätt till funktion postData med token som parameter.
+ 
   if (userJson.jwt) return userJson.jwt
   else {
-    //Inloggningen har misslyckats. Skriv ut errormeddelande från Strapi.js
+  
     let errMessage = userJson.error.message
 
     document.getElementById("userError").innerText = errMessage
@@ -113,20 +93,19 @@ async function getToken() {
 }
 
 async function postData() {
-  //Anropa GetToken() för att få en inloggnings-nyckel.
-  //Om detta misslyckas, avbryt funktionen.
+
   let token = await getToken()
   if (!token) return
 
-  //URL till Strapi Pokemon collection.
+
   const urlPokemon = "http://localhost:1337/api/pokemons/"
 
-  // Hämtar data från fält
+
   const name = document.getElementById("name").value
   const type = document.getElementById("type").value
   const level = document.getElementById("level").value
 
-  //Skapa ett objekt med data inkluderat.
+
   let pokemonObjekt = {
     data: {
       name: name,
@@ -135,7 +114,7 @@ async function postData() {
     }
   }
 
-  //Anropar API med pokemonObjekt
+ 
   let pokemonResponse = await fetch(urlPokemon, {
     method: "POST",
     headers: {
@@ -151,10 +130,9 @@ async function postData() {
   await getDataFromStrapi()
 }
 
-//Funktioner för validering
-//Validering av User Input
+
 function userValidate(comp) {
-  // 1. Fältet måste vara ifyllt
+
 
   let valid = true
 
@@ -163,7 +141,7 @@ function userValidate(comp) {
     valid = false
   }
 
-  //Check on lyckad validering
+
   if (!valid) {
     document.getElementById("userError").innerText =
       "Du måste fylla i ett användarnamn!"
@@ -174,18 +152,18 @@ function userValidate(comp) {
   }
 }
 
-//Validering av Password input
+
 function passValidate(comp) {
-  // 1. Fältet måste vara minst 5 tecken eller längre
+
 
   let valid = true
 
   if (comp.value.length <= 4) {
-    //Misslyckad validering
+   
     valid = false
   }
 
-  //Check on lyckad validering
+ 
   if (!valid) {
     document.getElementById("passwordError").innerText =
       "Lösenordet måste vara minst 5 tecken långt!"
@@ -196,17 +174,17 @@ function passValidate(comp) {
   }
 }
 
-//funktion för validering av inloggninfsförsök
+
 function validateLogin() {
-  //Variabel
+
   let valid = true
 
-  //Validate Användarnamn
+ 
   if (!userValidate(document.getElementById("user"))) {
     valid = false
   }
 
-  //Validate Password
+
   if (!passValidate(document.getElementById("pass"))) {
     valid = false
   }
@@ -214,24 +192,22 @@ function validateLogin() {
   return valid
 }
 
-//Funktion för validering av Pokemon Name
-function pokemonNameValidate(comp) {
-  // 1. Fältet måste innehålla ett värde
-  // 2. Fältet får inte vara ett nummer
 
+function pokemonNameValidate(comp) {
+ 
   let valid = true
 
-  //CHeck om value är större än 0
+
   if (comp.value.length == 0) {
-    //Felaktig validering
+    
     valid = false
     document.getElementById("pokeNameError").innerText =
       "Pokemon Name måste vara ifyllt."
   }
 
-  //CHeck att värdet inte är ett nummer
+
   if (!isNaN(comp.value) && comp.value.length != 0) {
-    //Felaktig validering
+   
     valid = false
     document.getElementById("pokeNameError").innerText =
       "Namnet får inte vara ett nummer."
@@ -244,78 +220,69 @@ function pokemonNameValidate(comp) {
   return valid
 }
 
-//FUnktion för validering av Pokemon
+
 function validatePokemon() {
   let valid = true
 
-  //Validate PokemonName
+ 
   if (!pokemonNameValidate(document.getElementById("name"))) {
     valid = false
   }
 
-  //TODO - Skapa validering för Type och Level
 
   return valid
 }
 
-//Genererat tabellrad med det inkludera objektet. Skapar TH rad om header=true
+
 function generateRow(obj, objId, header) {
   let output = "<tr>"
   let forbiddenParameters = ["createdAt", "updatedAt", "publishedAt"]
 
-  //For in loop för att gå igenom alla parametrar i obj
-  for (x in obj) {
-    /*
-        x = parameterns namn
-        obj[x] = parameterns värde
-        */
 
-    //Kontrollera att x är en tillåten parameter.
-    // Keyword Continue går vidare till nästa parameter i loopen
-    //Fungerar också i en ForEach loop.
+  for (x in obj) {
+
     if (forbiddenParameters.includes(x)) continue
 
     if (header) output += `<th>${x}</th>`
     else output += `<tbody><td>${obj[x]}</td></tbody>`
   }
 
-  //Skapa update och Delete knapp för TD rad
+ 
   if (!header) {
-    //URL för den specifika posten
+   
     let postURL = `http://localhost:1337/api/pokemons/${objId}`
 
     output += `<td><button onclick="updatePost('${postURL}');">Update Post</button></td>`
     output += `<td><button onclick="deletePost('${postURL}');">Delete Post</button></td>`
   }
 
-  //Stänga <tr> taggen
+ 
   output += "</tr>"
 
   return output
 }
 
 async function updatePost(url) {
-  //Hämta Token från GetToken()
-  //Om ingen Token returneras, avbryt funktionen
+
   let token = await getToken()
   if (!token) return
 
-  // Hämtar data från fält
+
   const name = document.getElementById("name").value
   const type = document.getElementById("type").value
   const level = document.getElementById("level").value
 
-  //Skapa ett objekt med data inkluderat.
+  
   let pokemonObjekt = {
     data: {}
   }
 
-  //Fyller upp Data med parameter-värden
+
   if (name) pokemonObjekt.data["name"] = name
   if (type) pokemonObjekt.data["type"] = type
   if (level) pokemonObjekt.data["level"] = level
 
-  //Anropar API med pokemonObjekt
+
   await fetch(url, {
     method: "PUT",
     headers: {
@@ -325,18 +292,16 @@ async function updatePost(url) {
     body: JSON.stringify(pokemonObjekt)
   })
 
-  //Anropa "GetDataFromStrapi" för att skriva ut ny tabell
+ 
   await getDataFromStrapi()
 }
 
 async function deletePost(url) {
-  //Hämta Token från GetToken()
-  //Om ingen Token returneras, avbryt funktionen
+
   let token = await getToken()
   if (!token) return
 
-  //Anropar API med inloggningsdata.
-  //Inkluderar Method och Headers
+
   await fetch(url, {
     method: "DELETE",
     headers: {
@@ -345,6 +310,6 @@ async function deletePost(url) {
     }
   })
 
-  //Anropa "GetDataFromStrapi" för att skriva ut ny tabell
+  
   await getDataFromStrapi()
 }
